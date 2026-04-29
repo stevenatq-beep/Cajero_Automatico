51f7524ca783086b0b877de6cca8ecd239b5623f
Stnvna <stevenatq@gmail.com>
2026-04-29 07:57:44 -0400

    Implementar autenticacion basica

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char username[20];
+    char password[20];
+    double balance;
+} User;
 
 int main(void) {
-    printf("Cajero Automatico Digital\n");
-    printf("Proyecto en construccion...\n");
+    User users[3] = {
+        {"steven", "1234", 1500.00},
+        {"ana", "2026",  800.00},
+        {"juan", "qwerty", 300.00}
+    };
+
+    printf("Sistema listo con usuarios registrados.\n");
     return 0;
 }
